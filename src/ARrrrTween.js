94c8bcb2f40538a00482358aa9209ff385141818
Nevin Wouters <nevin-wouters@hotmail.com>
2018-09-19 13:52:21 +0200

    Added onStart, onComplete and onIteration callbacks

--- src/ARrrrTween.js
+++ src/ARrrrTween.js
@@ -49,7 +49,10 @@ export default class {
 			mirror: false,
 			ease: "easeInOutCubic",
 			delay: 0,
-			autoplay: true
+			autoplay: true,
+			onComplete: function(){},
+			onIteration: function(){},
+			onStart: function(){}
 		}
 
 		this.animations = [];
@@ -112,173 +115,216 @@ export default class {
 		this.offset.transform.scaleX = this.object.transform.scaleX.lastValue;
 		this.offset.transform.scaleY = this.object.transform.scaleY.lastValue;
 		this.offset.transform.scaleZ = this.object.transform.scaleZ.lastValue;
-		this.offset.material.opacity = this.object.material.opacity.lastValue;
+		if(this.object.material != null) {
+			this.offset.material.opacity = this.object.material.opacity.lastValue;
+		}
 		
 		if (Array.isArray(this.values) == false) {
 			this.EvaluateData(this.values);
-
+			
 			if(this.autoplay) {
 				this.StartPlaying();
 			}
 			return;
 		}
-
+		
 		this.values.forEach(valuesElement => {
 			this.EvaluateData(valuesElement)
 		});
-
+		
 		if(this.autoplay) {
 			this.StartPlaying();
 		}
 	}
 
 	AssignSignals() {
-		var x = Reactive.val(this.offset.transform.x);
-		var y = Reactive.val(this.offset.transform.y);
-		var z = Reactive.val(this.offset.transform.z);
+		//Check if UI
+		if(this.object.bounds != null) {
+			var x = Reactive.val(this.offset.transform.x);
+			var y = Reactive.val(this.offset.transform.y);
+			var z = Reactive.val(this.offset.transform.z);
 
-		var useX = false;
-		var useY = false;
-		var useZ = false;
+			var useX = false;
+			var useY = false;
+			var useZ = false;
 
-		var xSizeOffset = Reactive.val(0);
-		var ySizeOffset = Reactive.val(0);
-		var zSizeOffset = Reactive.val(0);
+			var xSizeOffset = Reactive.val(0);
+			var ySizeOffset = Reactive.val(0);
+			var zSizeOffset = Reactive.val(0);
 
-		var zRotationOffsetX = Reactive.val(0);
-		var zRotationOffsetY = Reactive.val(0);
+			var zRotationOffsetX = Reactive.val(0);
+			var zRotationOffsetY = Reactive.val(0);
 
-		var xRotationOffsetY = Reactive.val(0);
-		var xRotationOffsetZ = Reactive.val(0);
+			var xRotationOffsetY = Reactive.val(0);
+			var xRotationOffsetZ = Reactive.val(0);
 
-		var yRotationOffsetX = Reactive.val(0);
-		var yRotationOffsetZ = Reactive.val(0);
+			var yRotationOffsetX = Reactive.val(0);
+			var yRotationOffsetZ = Reactive.val(0);
 
-		//Assign signals and calculate offset
-		this.animations.forEach(animation => {
-			//MoveX
-			if (animation.id == "x") {
-				x = animation.signal;
-				useX = true;
-			}
-			//MoveY
-			if (animation.id == "y") {
-				y = animation.signal;
-				useY = true;
-			}
-			//MoveZ
-			if (animation.id == "z") {
-				z = animation.signal;
-				useZ = true;
-			}
+			//Assign signals and calculate offset
+			this.animations.forEach(animation => {
+				//MoveX
+				if (animation.id == "x") {
+					x = animation.signal;
+					useX = true;
+				}
+				//MoveY
+				if (animation.id == "y") {
+					y = animation.signal;
+					useY = true;
+				}
+				//MoveZ
+				if (animation.id == "z") {
+					z = animation.signal;
+					useZ = true;
+				}
 
-			//RotationX
-			if (animation.id == "rotationX") {
-				useY = true;
-				useZ = true;
+				//RotationX
+				if (animation.id == "rotationX") {
+					useY = true;
+					useZ = true;
 
-				var scaleY = Reactive.val(1);
+					var scaleY = Reactive.val(1);
 
-				this.animations.forEach(element => {
-					if (element.id == "scaleY") {
-						scaleY = element.signal;
-					}
-				});
+					this.animations.forEach(element => {
+						if (element.id == "scaleY") {
+							scaleY = element.signal;
+						}
+					});
 
-				var angle = 0;
-				var h = 0;
+					var angle = 0;
+					var h = 0;
 
-				h = this.object.bounds.height.div(2).mul(scaleY);
+					h = this.object.bounds.height.div(2).mul(scaleY);
 
-				angle = Reactive.atan2(this.object.bounds.height.div(2).mul(scaleY).mul(1), 0);
+					angle = Reactive.atan2(this.object.bounds.height.div(2).mul(scaleY).mul(1), 0);
 
-				xRotationOffsetZ = Reactive.cos(animation.signal.sub(angle)).mul(h).mul(-1);
-				xRotationOffsetY = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.height.div(2).mul(scaleY));
-				this.object.transform.rotationX = animation.signal;
-			}
+					xRotationOffsetZ = Reactive.cos(animation.signal.sub(angle)).mul(h).mul(-1);
+					xRotationOffsetY = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.height.div(2).mul(scaleY));
+					this.object.transform.rotationX = animation.signal;
+				}
 
-			//RotationY
-			if (animation.id == "rotationY") {
-				useX = true;
-				useZ = true;
+				//RotationY
+				if (animation.id == "rotationY") {
+					useX = true;
+					useZ = true;
 
-				var scaleX = Reactive.val(1);
-				this.animations.forEach(element => {
-					if (element.id == "scaleX") {
-						scaleX = element.signal;
-					}
-				});
+					var scaleX = Reactive.val(1);
+					this.animations.forEach(element => {
+						if (element.id == "scaleX") {
+							scaleX = element.signal;
+						}
+					});
 
-				var angle = 0;
-				var h = 0;
+					var angle = 0;
+					var h = 0;
+					
+					h = this.object.bounds.width.div(2).mul(scaleX);
+					
+					angle = Reactive.atan2(this.object.bounds.width.div(2).mul(scaleX).mul(1), 0);
+
+					yRotationOffsetZ = Reactive.cos(animation.signal.sub(angle)).mul(h);
+					yRotationOffsetX = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.width.div(2).mul(scaleX));
+					this.object.transform.rotationY = animation.signal;
+				}
+
+				//RotationZ
+				if (animation.id == "rotationZ") {
+					useX = true;
+					useY = true;
+
+					var scaleX = Reactive.val(1);
+					var scaleY = Reactive.val(1);
+
+					this.animations.forEach(element => {
+						if (element.id == "scaleX") {
+							scaleX = element.signal;
+						}
+						if (element.id == "scaleY") {
+							scaleY = element.signal;
+						}
+					});
+
+					var angle = 0;
+					var h = 0;
+
+					h = Reactive.sqrt(Reactive.add(this.object.bounds.height.div(2).mul(scaleY).pow(2), this.object.bounds.width.div(2).mul(scaleX).pow(2)));
+
+					angle = Reactive.atan2(this.object.bounds.height.div(2).mul(scaleY).mul(1), this.object.bounds.width.div(2).mul(scaleX).mul(-1));
+
+					zRotationOffsetX = Reactive.cos(animation.signal.sub(angle)).mul(h).add(this.object.bounds.width.div(2).mul(scaleX));
+					zRotationOffsetY = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.height.div(2).mul(scaleY));
+					this.object.transform.rotationZ = animation.signal;
+				}
 				
-				h = this.object.bounds.width.div(2).mul(scaleX);
-				
-				angle = Reactive.atan2(this.object.bounds.width.div(2).mul(scaleX).mul(1), 0);
+				//ScaleX
+				if (animation.id == "scaleX") {
+					useX = true;
 
-				yRotationOffsetZ = Reactive.cos(animation.signal.sub(angle)).mul(h);
-				yRotationOffsetX = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.width.div(2).mul(scaleX));
-				this.object.transform.rotationY = animation.signal;
+					xSizeOffset = this.object.bounds.width.div(2/this.offset.transform.scaleX).sub(this.object.bounds.width.div(2).mul(animation.signal));
+					this.object.transform.scaleX = animation.signal;
+				}
+				//ScaleY
+				if (animation.id == "scaleY") {
+					useY = true;
+
+					ySizeOffset = this.object.bounds.height.div(2/this.offset.transform.scaleY).sub(this.object.bounds.height.div(2).mul(animation.signal));
+					this.object.transform.scaleY = animation.signal;
+				}
+
+				//Opacity
+				if(animation.id == "opacity") {
+					this.object.material.opacity = animation.signal;
+				}
+			});
+
+			if(useX) {
+				this.object.transform.x = x.add(xSizeOffset).add(zRotationOffsetX).add(yRotationOffsetX);
 			}
-
-			//RotationZ
-			if (animation.id == "rotationZ") {
-				useX = true;
-				useY = true;
-
-				var scaleX = Reactive.val(1);
-				var scaleY = Reactive.val(1);
-
-				this.animations.forEach(element => {
-					if (element.id == "scaleX") {
-						scaleX = element.signal;
-					}
-					if (element.id == "scaleY") {
-						scaleY = element.signal;
-					}
-				});
-
-				var angle = 0;
-				var h = 0;
-
-				h = Reactive.sqrt(Reactive.add(this.object.bounds.height.div(2).mul(scaleY).pow(2), this.object.bounds.width.div(2).mul(scaleX).pow(2)));
-
-				angle = Reactive.atan2(this.object.bounds.height.div(2).mul(scaleY).mul(1), this.object.bounds.width.div(2).mul(scaleX).mul(-1));
-
-				zRotationOffsetX = Reactive.cos(animation.signal.sub(angle)).mul(h).add(this.object.bounds.width.div(2).mul(scaleX));
-				zRotationOffsetY = Reactive.sin(animation.signal.sub(angle)).mul(h).add(this.object.bounds.height.div(2).mul(scaleY));
-				this.object.transform.rotationZ = animation.signal;
+			if(useY){
+				this.object.transform.y = y.add(ySizeOffset).add(zRotationOffsetY).add(xRotationOffsetY);
 			}
-			
-			//ScaleX
-			if (animation.id == "scaleX") {
-				useX = true;
-
-				xSizeOffset = this.object.bounds.width.div(2/this.offset.transform.scaleX).sub(this.object.bounds.width.div(2).mul(animation.signal));
-				this.object.transform.scaleX = animation.signal;
+			if(useZ) {
+				this.object.transform.z = z.add(xRotationOffsetZ).add(yRotationOffsetZ);
 			}
-			//ScaleY
-			if (animation.id == "scaleY") {
-				useY = true;
-
-				ySizeOffset = this.object.bounds.height.div(2/this.offset.transform.scaleY).sub(this.object.bounds.height.div(2).mul(animation.signal));
-				this.object.transform.scaleY = animation.signal;
-			}
-
-			//Opacity
-			if(animation.id == "opacity") {
-				this.object.material.opacity = animation.signal;
-			}
-		});
-
-		if(useX) {
-			this.object.transform.x = x.add(xSizeOffset).add(zRotationOffsetX).add(yRotationOffsetX);
-		}
-		if(useY){
-			this.object.transform.y = y.add(ySizeOffset).add(zRotationOffsetY).add(xRotationOffsetY);
-		}
-		if(useZ) {
-			this.object.transform.z = z.add(xRotationOffsetZ).add(yRotationOffsetZ);
+		} else {
+			this.animations.forEach(animation => {
+				//Position
+				if(animation.id == "x") {
+					this.object.transform.x = animation.signal;
+				}
+				if(animation.id == "y") {
+					this.object.transform.y = animation.signal;
+				}
+				if(animation.id == "z") {
+					this.object.transform.z = animation.signal;
+				}
+				//Rotation
+				if(animation.id == "rotationX") {
+					this.object.transform.rotationX = animation.signal;
+				}
+				if(animation.id == "rotationY") {
+					this.object.transform.rotationY = animation.signal;
+				}
+				if(animation.id == "rotationZ") {
+					this.object.transform.rotationZ = animation.signal;
+				}
+				//Scale
+				if(animation.id == "scaleX") {
+					this.object.transform.scaleX = animation.signal;
+				}
+				if(animation.id == "scaleY") {
+					this.object.transform.scaleY = animation.signal;
+				}
+				if(animation.id == "scaleZ") {
+					this.object.transform.scaleZ = animation.signal;
+				}
+				//Materials
+				//Opacity
+				if(animation.id == "opacity") {
+					this.object.material.opacity = animation.signal;
+				}
+			});
 		}
 	}
 
@@ -297,6 +343,9 @@ export default class {
 		var mirror = this.defaultControls.mirror;
 		var ease = this.defaultControls.ease;
 		var delay = this.defaultControls.delay;
+		var onComplete = this.defaultControls.onComplete;
+		var onIteration = this.defaultControls.onIteration;
+		var onStart = this.defaultControls.onStart;
 
 		var signal = Reactive.val(0);
 
@@ -360,11 +409,13 @@ export default class {
 			end = startEnd.end;
 		}
 		//Material
-		if (data.opacity != null) {
-			id = "opacity";
-			var startEnd = this.CheckValue(data.opacity, this.offset.material.opacity);
-			start = startEnd.start;
-			end = startEnd.end;
+		if(this.object.material != null) {
+			if (data.opacity != null) {
+				id = "opacity";
+				var startEnd = this.CheckValue(data.opacity, this.offset.material.opacity);
+				start = startEnd.start;
+				end = startEnd.end;
+			}
 		}
 
 		//Controls
@@ -392,6 +443,18 @@ export default class {
 		if(data.delay != null) {
 			delay = data.delay;
 		}
+		//OnComplete
+		if(data.onComplete != null) {
+			onComplete = data.onComplete;
+		}
+		//OnIteration
+		if(data.onIteration != null) {
+			onIteration = data.onIteration;
+		}
+		//OnStart
+		if(data.onStart != null) {
+			onStart = data.onStart;
+		}
 
 		var AnimationDriver = Animation.timeDriver({ durationMilliseconds: duration, loopCount: loopCount, mirror: mirror });
 		var AnimationValue = Animation.samplers[ease](start, end);
@@ -399,7 +462,10 @@ export default class {
 
 		signal = Animate;
 
-		this.animations.push({ id: id, signal: signal, duration: duration, delay: delay, driver: AnimationDriver });
+		AnimationDriver.onCompleted().subscribe(onComplete);
+		AnimationDriver.onAfterIteration().subscribe(onIteration);
+
+		this.animations.push({ id: id, signal: signal, duration: duration, delay: delay, driver: AnimationDriver, onStart: onStart });
 	}
 
 	CheckValue(data ,offset, isRotation) {
@@ -485,9 +551,11 @@ export default class {
 
 				if(anim.delay == 0) {
 					anim.driver.start();
+					anim.onStart();
 				} else {
 					Time.setTimeout(function(){
 						anim.driver.start();
+						anim.onStart();
 					},anim.delay);
 				}
 			});
