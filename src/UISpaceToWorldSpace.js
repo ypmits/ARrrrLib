6afd8f0fe015325c839745f80172ebed796c5678
Nevin Wouters <nevin-wouters@hotmail.com>
2018-09-25 14:25:59 +0200

    Added UI to worldspace class

---
+++ src/UISpaceToWorldSpace.js
@@ -0,0 +1,114 @@
+import Reactive from 'Reactive';
+import Scene from 'Scene';
+import console from 'Diagnostics';
+
+export default class {
+    constructor(UIObject, parentCanvas, worldObject, onlyInitialPosition) {
+        var worldWidth;
+		var worldHeight;
+		var aspect;
+		var canvasHeight = parentCanvas.height.lastValue;
+        var canvasWidth = parentCanvas.width.lastValue;
+        
+        if(onlyInitialPosition == null) {
+            onlyInitialPosition = false;
+        }
+        
+		var startedInPortrait = true;
+        
+		console.log("width: " + canvasWidth + " - height: " + canvasHeight);
+		var landscape = this.isLandscapeMode();
+		landscape.monitor().subscribe(function() {
+            //Landscape
+			if(landscape.lastValue) {
+                if(startedInPortrait) {
+                    aspect = parentCanvas.height.lastValue / parentCanvas.width.lastValue;
+					worldWidth = Reactive.val(66.6);
+					worldHeight = worldWidth.div(aspect);
+                    
+					canvasHeight = parentCanvas.width.lastValue;
+					canvasWidth = parentCanvas.height.lastValue;
+				} else {
+                    aspect = parentCanvas.width.lastValue / parentCanvas.height.lastValue;
+					worldWidth = Reactive.val(66.6);
+					worldHeight = worldWidth.div(aspect);
+                    
+					canvasHeight = parentCanvas.height.lastValue;
+					canvasWidth = parentCanvas.width.lastValue;
+				}
+                
+				console.log("width: " + canvasWidth + " - height: " + canvasHeight + " - aspect: " + aspect);
+                //Portrait
+			} else {
+                if(startedInPortrait) {
+                    aspect = parentCanvas.width.lastValue / parentCanvas.height.lastValue;
+					worldHeight = Reactive.val(50);
+					worldWidth = worldHeight.mul(aspect);
+					
+					canvasHeight = parentCanvas.height.lastValue;
+					canvasWidth = parentCanvas.width.lastValue;
+				} else {
+                    aspect = parentCanvas.height.lastValue / parentCanvas.width.lastValue;
+					worldHeight = Reactive.val(50);
+					worldWidth = worldHeight.mul(aspect);
+                    
+					canvasHeight = parentCanvas.width.lastValue;
+					canvasWidth = parentCanvas.height.lastValue;
+				}
+                
+				console.log("width: " + canvasWidth + " - height: " + canvasHeight + " - aspect: " + aspect);
+			}
+			//Set Values
+			var centerOffsetY = UIObject.bounds.height.div(2);
+			var centerOffsetX = UIObject.bounds.width.div(2);
+            
+            if(onlyInitialPosition == false) {
+                var positionX = UIObject.bounds.x.sum(centerOffsetX).sub(UIObject.transform.x).div(canvasWidth).mul(worldWidth.mul(-1)).sum(worldWidth.div(2));
+                var positionY = UIObject.bounds.y.sum(centerOffsetY).sub(UIObject.transform.y).div(canvasHeight).mul(worldHeight.mul(-1)).sum(worldHeight.div(2));
+            } else {
+                var positionX = UIObject.bounds.x.sum(centerOffsetX).div(canvasWidth).mul(worldWidth.mul(-1)).sum(worldWidth.div(2));
+                var positionY = UIObject.bounds.y.sum(centerOffsetY).div(canvasHeight).mul(worldHeight.mul(-1)).sum(worldHeight.div(2));
+            }
+
+			worldObject.transform.x = positionX;
+			worldObject.transform.y = positionY;
+			worldObject.transform.z = 0;
+		}.bind(this));
+        
+		//If PortraitMode
+		if(parentCanvas.width.lastValue >= parentCanvas.height.lastValue) {
+            startedInPortrait = false;
+			aspect = parentCanvas.width.lastValue / parentCanvas.height.lastValue;
+			worldWidth = Reactive.val(66.6);
+			worldHeight = worldWidth.div(aspect);
+		} else {
+            startedInPortrait = true;
+			aspect = parentCanvas.width.lastValue / parentCanvas.height.lastValue;
+			worldHeight = Reactive.val(50);
+			worldWidth = worldHeight.mul(aspect);
+		}
+        
+		//Set first time
+		var centerOffsetY = UIObject.bounds.height.div(2);
+		var centerOffsetX = UIObject.bounds.width.div(2);
+        
+        if(onlyInitialPosition == false) {
+            var positionX = UIObject.bounds.x.sum(centerOffsetX).sub(UIObject.transform.x).div(canvasWidth).mul(worldWidth.mul(-1)).sum(worldWidth.div(2));
+            var positionY = UIObject.bounds.y.sum(centerOffsetY).sub(UIObject.transform.y).div(canvasHeight).mul(worldHeight.mul(-1)).sum(worldHeight.div(2));
+        } else {
+            var positionX = UIObject.bounds.x.sum(centerOffsetX).div(canvasWidth).mul(worldWidth.mul(-1)).sum(worldWidth.div(2));
+            var positionY = UIObject.bounds.y.sum(centerOffsetY).div(canvasHeight).mul(worldHeight.mul(-1)).sum(worldHeight.div(2));
+        }
+        
+		worldObject.transform.x = positionX;
+		worldObject.transform.y = positionY;
+		worldObject.transform.z = 0;
+    }
+
+    isLandscapeMode() {
+		var camera = Scene.root.find("Camera");
+		var landscape = Reactive.ge(camera.focalPlane.width,camera.focalPlane.height);
+
+		return landscape;
+	}
+}
