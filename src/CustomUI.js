fbe884c8c6ec315c70f6b14fb6bc91c1c87ab8e4
Nevin Wouters <nevin-wouters@hotmail.com>
2018-10-03 15:45:34 +0200

    Repaired the UIToWorldSpace Function

---
+++ src/CustomUI.js
@@ -0,0 +1,12 @@
+export default {
+
+	UIToWorldSpace(worldObject, UIObject, canvas, onlyDefaultPosition) {
+			if(onlyDefaultPosition) {
+				worldObject.transform.x = UIObject.bounds.x.sub(canvas.width.lastValue/2).add(UIObject.bounds.width.div(2));
+				worldObject.transform.y = UIObject.bounds.y.sub(canvas.height.lastValue/2).add(UIObject.bounds.height.div(2)).mul(-1);
+			} else {
+				worldObject.transform.x = UIObject.transform.x.add(UIObject.bounds.x.sub(canvas.width.lastValue/2).add(UIObject.bounds.width.div(2)));
+				worldObject.transform.y = UIObject.transform.y.sub(UIObject.bounds.y.sub(canvas.height.lastValue/2).add(UIObject.bounds.height.div(2)));
+			}
+		}
+}
