8860270925a546e97aee98862962c7f9957768e4
Nevin Wouters <nevin-wouters@hotmail.com>
2018-10-22 11:05:21 +0200

    OnStart after every iteration

--- src/ARrrrTween.js
+++ src/ARrrrTween.js
@@ -521,6 +521,7 @@ export default class {
 			}
 		}.bind(this));
 		AnimationDriver.onAfterIteration().subscribe(onIteration);
+		AnimationDriver.onStart().subscribe(onStart);
 
 		this.animations.push({ id: id, signal: signal, duration: duration, delay: delay, driver: AnimationDriver, onStart: onStart });
 	}
