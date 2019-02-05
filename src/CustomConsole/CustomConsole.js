daaf3e5b6f77b0bc05a507579b84fd89d33190a7
Nevin Wouters <nevin-wouters@hotmail.com>
2019-02-05 16:45:07 +0100

    Added CustomConsole to the library + documentation

---
+++ src/CustomConsole/CustomConsole.js
@@ -0,0 +1,202 @@
+import Time from 'Time';
+
+export default class Console {
+    
+    constructor(textfield, options) {
+        
+        //#region Settings
+        var collapse = (options != null && options.collapse != null)?options.collapse:false;
+        var maxLines = (options != null && options.maxLines != null)?options.maxLines:5;
+        var keepLog = (options != null && options.keepLog != null)?options.keepLog:false;
+        //#endregion
+
+        //#region Fields
+        var startAt = 0;
+        var scrollStartAt;
+        var lines = [];
+        textfield.text = "";
+
+        var autoRefreshInterval;
+        //#endregion
+
+        //#region Public Methods
+        this.log = function(string) {            
+            switch(typeof string) {
+                case "number":
+                case "string":
+                case "boolean":
+                    if(collapse) {
+                        for(var i = 0; i < lines.length; i++) {
+                            if(lines[i].type == "logObject") {
+                                if(lines[i].string == string) {
+                                    lines[i].addCount();
+                                    refreshConsole();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    var log = new logObject(string);
+                    lines.push(log);
+                    refreshConsole();
+                break;
+             case "object":
+                    var log = new logObject("[object]");
+                    lines.push(log);
+                    refreshConsole();
+                break;
+                case "function":
+                    try {
+                        string.pinLastValue();
+                        var log = new logObject(string.pinLastValue());
+                        lines.push(log);
+                        refreshConsole();
+                    } catch(err) {
+                        var log = new logObject("[function]");
+                        lines.push(log);
+                        refreshConsole();
+                    }
+                break;
+                case "undefined":
+                    var log = new logObject("[undefined]");
+                    lines.push(log);
+                    refreshConsole();
+                break;
+                default:
+                    var log = new logObject("[type not found]");
+                    lines.push(log);
+                    refreshConsole();
+                break;
+            }
+        }
+
+        this.watch = function(name,signal) {
+            if(typeof signal == "function") {
+                try {
+                    signal.pinLastValue();
+                    var log = new signalObject(name,signal);
+                    lines.push(log);
+
+                    refreshConsole();
+
+                    if(autoRefreshInterval == null) {
+                        autoRefreshInterval = Time.setInterval(()=>{refreshConsole()},100);
+                    }
+                } catch(err) {
+                    var log = new logObject(name + ": [not a signal]");
+                    lines.push(log);
+                    refreshConsole();
+                }
+                
+            } else {
+                var log = new logObject(name + ": [not a signal]");
+                lines.push(log);
+                refreshConsole();
+            }
+        }
+
+        this.clear = function() {
+            scrollStartAt = null;
+            textfield.text = "";
+            lines = [];
+            startAt = 0;
+        }
+
+        this.scrollToTop = function() {
+            scrollStartAt = 0;
+        }
+
+        this.scrollUp = function() {
+            if(scrollStartAt != null) {
+                scrollStartAt--;
+            } else {
+                scrollStartAt = lines.length-maxLines-1;
+            }
+            if(scrollStartAt < 0) scrollStartAt = 0;
+        }
+        
+        this.scrollDown = function() {
+            if(scrollStartAt != null) {
+                scrollStartAt++;
+            } else {
+                scrollStartAt = lines.length-maxLines+1;
+            }
+            if(scrollStartAt > lines.length-maxLines) {
+                scrollStartAt = null;
+            }
+        }
+        
+        this.scrollToBottom = function() {
+            scrollStartAt = null;
+        }
+        //#endregion
+
+        // #region Private Methods
+        var refreshConsole = function() {
+            if(!keepLog) {
+                while(lines.length > maxLines) {
+                    lines.shift();
+                }
+            } else {
+                if(lines.length < maxLines) {
+                    startAt = 0;
+                } else {
+                    startAt = lines.length-maxLines;
+                }
+                // if(startAt)
+            }
+
+            var newText = "";
+
+            for(var i = (lines.length>maxLines)?maxLines-1:lines.length-1; i >= 0; i--) {
+                
+                var index = i + startAt;
+                if(scrollStartAt != null) {
+                    index = i + scrollStartAt;
+                }
+
+                if(lines[index].type == "logObject") {
+                    newText += ">>>"+((lines[index].count<=1)?"   ":"["+lines[index].count+"]")+" "+ lines[index].string + "\n";
+                } else if (lines[index].type = "signalObject"){
+                    newText += "<O>    " +lines[index].name+":"+ lines[index].signal.pinLastValue() + "\n";
+                }
+            }
+            textfield.text = newText;
+
+            var containsSignal = false;
+            for(var i = lines.length-1; i >= 0; i--) {
+                if(lines[i].type == "signalObject") {
+                    containsSignal = true;   
+                }
+            }
+            if(!containsSignal) {
+                if(autoRefreshInterval != null) {
+                    Time.clearInterval(autoRefreshInterval);
+                }
+            }
+        }
+        //#endregion
+        
+        //#region Classes
+        class logObject {
+            constructor(string) {
+                this.type = "logObject";
+                this.string = string;
+                this.count = 1;
+            }
+
+            addCount() {
+                this.count++;
+            }
+        }
+
+        class signalObject {
+            constructor(name,signal) {
+                this.type = "signalObject";
+                this.name = name;
+                this.signal = signal;
+            }
+        }
+        //#endregion
+    }
+}
---
+++ src/CustomConsole/README.md
@@ -0,0 +1,65 @@
+# CustomConsole
+### Introduction
+The Custom Console is a library to create a on screen console for debugging.
+This is the documentation for a Custom Console API for SparkAR Studio. To start using the API you first have to import it from FARlib.
+
+
+```javascript
+import CustomConsole from '[PATH]/FARLib/src/CustomConsole/CustomConsole';
+```
+
+### Code Examples
+To start using the console you first have to create a new console like in the first example.
+```javascript
+//Syntax
+var console = new CustomConsole(textObject, settings);
+
+//Example
+var console = new CustomConsole(textObject, {
+   collapse:true, 
+   maxLines: 3,
+   keepLog: true
+});
+
+//Will add '>> hello world' to the console
+console.log("hello world");
+
+//Will add '<O> faceX: 0.434275532' to the console
+//The variable will update in realtime
+console.watch("faceX",face.cameraTransform.x);
+```
+
+### Settings
+
+The settings are of the type "object".
+Settings | description
+--- | ---
+collapse | *Type: bool - Default: false* <br> If collapse is equal to true the console will collapse log items with the same value into a single entry with a count in front of it. <br><br> *Example:*<br>>> [3] consoleEntry<br>This means "consoleEntry" is logged three times
+maxLines | *Type: int - Default: 5* <br> The maxLines setting are the amount of times shown inside of the text object. If the maxLines amount is bigger than what fits inside of the text field it will show "..." at the end of the last entry.
+keepLog | *Type: bool - Default: false* <br> When keepLog is equal to true all entries will be saved until the console is cleared. This allows to scroll back to check previous logged items.
+
+### Methods
+
+Methods | description
+---|---
+log | *Parameters: Any variable* <br> *[supports Number, String, Boolean, Object, Function, Signal, undefined]* <br> Logging a variable will show it in the console
+watch | *Parameters: Name,Signal* <br> Watching a signal will show the update the signal value inside of the console with the given name as index
+clear | Removes all the console entries
+scrollToTop | Scrolls back to the first console entry, autoscroll will be disabled
+scrollUp | Scrolls back one console entry, autoscroll will also be disabled
+scrollDown | Scrolls forward one console entry, when reaching the newest entry autoscroll will be enabled again
+scrollToBottom | Scrolls back to the last console entry and enabled autoscroll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
