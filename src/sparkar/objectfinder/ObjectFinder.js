52dcb04a0a9480ee78e85dc184c19c0a2803014c
Dave Lenz <ypmits@gmail.com>
2020-04-03 13:21:08 +0200

    Updated ObjectFinder

--- src/sparkar/objectfinder/ObjectFinder.js
+++ src/sparkar/objectfinder/ObjectFinder.js
@@ -1,35 +1,68 @@
 const Scene = require('Scene');
 const Diagnostics = require('Diagnostics');
 
-export default class ObjectFinder {
-    // ------------Example------------
-    // var object = ObjectFinder.find("objectToFind"); // Find a object in the Scene.root
-    // var object = ObjectFinder.find("childObjectToFind",object); // Use a SceneObject to indicate the parent object (recommended)
-    // var object = ObjectFinder.find("childObjectToFind","objectToFind"); // Use a string to indicate the parent object
-    // --------------End--------------
-    static find(object, inside = null) {
-        if(object == null) {
-            Diagnostics.log("[error] no object name given");
-        } else if (typeof object != "string") {
-            Diagnostics.log("[error] object type has to be a string");
-        } else {
-            try {
-                if(inside == null) {
-                    return Scene.root.find(object);
-                } else {
-                    if(typeof inside == "string") {
-                        return Scene.root.find(inside).find(object);
-                    } else {
-                        return inside.find(object);
-                    }
-                }
-            } catch(e) {
-                if(inside == null) {
-                    Diagnostics.log(`[error] could not find "${object}" in the scene`);
-                } else {
-                    Diagnostics.log(`[error] could not find "${object}" in ${(typeof inside == "string")?inside:inside.name}`);
-                }
-            }
-        }
-    }
+/**
+Searches for objects in the sceneview.
+
+ObjectFinder.find(object, inside):
+
+var object = ObjectFinder.find("objectToFind"); // Find a object in the Scene.root
+var object = ObjectFinder.find("childObjectToFind",object); // Use a SceneObject to indicate the parent object (recommended)
+var object = ObjectFinder.find("childObjectToFind","objectToFind"); // Use a string to indicate the parent object
+
+Returns a promise
+*/
+export default class ObjectFinder
+{
+	static find(object, inside = null)
+	{
+		if (object == null)
+		{
+			Diagnostics.log("[error] no object name given");
+			return null;
+		} else if (typeof object != "string")
+		{
+			Diagnostics.log("[error] object type has to be a string");
+			return null;
+		} else
+		{
+			try
+			{
+				if (inside == null) return Scene.root.findFirst(object);
+				else
+				{
+					if (typeof inside == "string") return Scene.root.findFirst(inside).findFirst(object);
+					else return inside.findFirst(object);
+				}
+			} catch (e) { return ObjectFinder.handleError(e, object, inside); }
+		}
+	}
+
+	static findAll(objectNames, inside = null, recursive = true)
+	{
+		if (objectNames == null)
+		{
+			Diagnostics.log("[error] no objectnames is given");
+			return [];
+		} else
+		{
+			try
+			{
+				if (inside == null) return Scene.root.findAll(objectNames, {recursive:recursive});
+				else
+				{
+					if (typeof inside == "string") return Scene.root.findAll(inside, {recursive:recursive}).findAll(objectNames, {recursive:recursive});
+					else return inside.findAll(objectNames, {recursive:recursive});
+				}
+			} catch (e) { return ObjectFinder.handleError(e, objectNames, inside); }
+		}
+	}
+
+	static handleError(e, objectNames, inside = null)
+	{
+		var base = `[error] could not find "${objectNames}" in `;
+		var where = (inside == null) ? "the scene" : `${(typeof inside == "string") ? inside : inside.name}`;
+		Diagnostics.log(base + where);
+		return base + where;
+	}
 }
--- src/sparkar/objectfinder/README.md
+++ src/sparkar/objectfinder/README.md
@@ -1,9 +1,25 @@
 # ObjectFinder
 ### Introduction
-Use **ObjectFinder** to replace the *Scene.root.find()* method.   
+Use **ObjectFinder** to replace the *Scene.root.find()* method.
+ObjectFinder is updated to ***v85*** and implements the new async-behaviours.
 ObjectFinder gives you better debug-information when an object is not found.
 ```javascript
-var otherObject = ObjectFinder.find("objectNameToFind"); // Find a object in the Scene.root
-var object = ObjectFinder.find("childObjectNameToFind", otherObject); // Use a SceneObject to indicate the parent object (recommended)
-var object = ObjectFinder.find("childObjectToFind", "objectToFind"); // Use a string to indicate the parent object
+// ObjectFinder to look for 1 object:
+ObjectFinder.find("IcoSphereHolder").then(element => {
+	log("[ObjectFinder test:find] Found the element: "+element);
+});
+
+// ObjectFinder to look for 1 object only inside another object:
+ObjectFinder.find("IcoSphereHolder", "othersphereHolder").then(element => {
+	log("[ObjectFinder test:find] Found the element: "+element);
+});
+
+// ObjectFinder to look for multiple objects:
+ObjectFinder.findAll("IcoSphere", null, true).then( elements => {
+	log(`[ObjectFinder test:findAll] Found ${elements.length}:`);
+	for (let i = 0; i < elements.length; i++) {
+		const element = elements[i];
+		log("    - sphere:"+element);
+	}
+});
 ```
